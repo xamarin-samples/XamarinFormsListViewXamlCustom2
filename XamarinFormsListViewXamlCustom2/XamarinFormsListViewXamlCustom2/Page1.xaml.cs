a38bdd0c0a9c446eb9f0ecd3e33be32008b8cc87
kobake <kobake@users.sourceforge.net>
2016-08-11 15:21:53 +0900

    Put ListView with custom ContentView onto XAML page, and bind data list

--- XamarinFormsListViewXamlCustom2/XamarinFormsListViewXamlCustom2/Page1.xaml.cs
+++ XamarinFormsListViewXamlCustom2/XamarinFormsListViewXamlCustom2/Page1.xaml.cs
@@ -13,6 +13,16 @@ namespace XamarinFormsListViewXamlCustom2
 		public Page1()
 		{
 			InitializeComponent();
+
+			// Make data list
+			List<ItemInfo> list = new List<ItemInfo>();
+			list.Add(new ItemInfo { Name = "Apple", Score = 100 });
+			list.Add(new ItemInfo { Name = "Banana", Score = 150 });
+			list.Add(new ItemInfo { Name = "Candy", Score = 80 });
+			list.Add(new ItemInfo { Name = "Drop", Score = -10 });
+
+			// Bind
+			this.BindingContext = list;
 		}
 	}
 }
